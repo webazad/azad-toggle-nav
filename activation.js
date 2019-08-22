5ba05ccfd9546fcb6414120bbe77771daa13cf4a
Azad <azad@azad.com>
2019-08-22 12:55:19 +0600

    Last

---
+++ activation.js
@@ -0,0 +1,6 @@
+;(function($){
+    $(document).ready(function(){
+        $('#menu-main-menu').slicknav();
+        alert('asdfr');
+    });
+})(jquery);
--- azad-toggle-nav.php
+++ azad-toggle-nav.php
@@ -17,7 +17,6 @@ if(! class_exists('Azad_Toggle_Nav')){
 		public static $instance = null;
         public function __construct(){
             add_action('wp_enqueue_scripts',array($this,'azad_public_scripts'));
-            add_action('wp_head',array($this,'azad_public_head'));
         }
         public function azad_public_scripts(){
 			wp_register_style( 'azad-style', plugins_url( 'style.css', __FILE__ ), array(), '12', 'all');
@@ -26,22 +25,15 @@ if(! class_exists('Azad_Toggle_Nav')){
             wp_enqueue_style( 'azad-style');
             wp_enqueue_style( 'azad-slicknav');
             
-			wp_register_script( 'azad-jquery', plugins_url( 'azad-jquery.min.js', __FILE__ ), array(), '23', false );
+            //wp_deregister_script('jquery');
+			///wp_register_script( 'azad-jquery', plugins_url( 'azad-jquery.min.js', __FILE__ ), array(), '23', true );
 			wp_register_script( 'azad-modernizr', plugins_url( 'modernizr.min.js', __FILE__ ), array(), '23', false );
-            wp_register_script( 'azad-slicknav', plugins_url( 'jquery.slicknav.js', __FILE__ ), array(), '23', true );
+            wp_register_script( 'azad-slicknav', plugins_url( 'jquery.slicknav.js', __FILE__ ), array('azad-jquery'), '23', true );
+            wp_register_script( 'azad-activation', plugins_url( 'activation.js', __FILE__ ), array('azad-jquery','azad-slicknav'), '23', true );
             
             wp_enqueue_script('azad-modernizr');
             wp_enqueue_script('azad-slicknav');
-        }
-        public function azad_public_head(){
-			?>
-                <script>
-                    $(document).ready(function(){
-                        //$('#menu').slicknav();
-                        alert('asdfr');
-                    });
-                </script>
-            <?php
+            wp_enqueue_script('azad-activation');
         }
         public static function _get_instance(){
             if(is_null(self::$instance) && ! isset(self::$instance) && ! (self::$instance instanceof self)){
