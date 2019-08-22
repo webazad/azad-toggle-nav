63a45c03edbe4407e86a9453d38e691fb09f8a35
Azad <azad@azad.com>
2019-08-22 10:59:15 +0600

    File aded.

---
+++ azad-toggle-nav.php
@@ -0,0 +1,13 @@
+<?php
+/* 
+Plugin Name: Azad Toggle Nav
+Description: Responsive nav
+Plugin URi: gittechs.com/plugin/azad-wp-starter-plugin 
+Author: Md. Abul Kalam Azad
+Author URI: gittechs.com/author
+Author Email: webdevazad@gmail.com
+Version: 0.0.0.1
+Text Domain: azad-toggle-nav
+*/ 
+// EXIT IF ACCESSED DIRECTLY
+defined('ABSPATH') || exit;
